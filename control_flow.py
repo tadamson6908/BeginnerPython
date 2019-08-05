43e1ce5a50207f8e8a3e9fdf33f2d0173e7219c9
Thomas Adamson <thomas.adamson20192@gmail.com>
2019-08-05 11:06:08 -0600

    Added new printline

--- control_flow.py
+++ control_flow.py
@@ -6,6 +6,20 @@ Learn about control Flow in python
 # if statement
 if True :
     print("It is true")
+if bool("eggs"):
+    print("Yes please!")
+if "eggs":
+    print("Yes, why not")
 
+#Flat is better than Nested
+h = 42
+if h > 50:
+    print("Greater than 50")
+    if h > 100:
+        print("greater than 100")
+elif h < 50 :
+    print("Less than 50")
+else:
+    print("Equals 50")
 
 print("Done")
