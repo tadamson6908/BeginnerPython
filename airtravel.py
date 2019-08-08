8d35b21dac3c514e74d776a7720a360013aa8217
thomasadamson <thomas.adamson20192@gmail.com>
2019-08-08 15:59:43 -0600

    added everything

--- airtravel.py
+++ airtravel.py
@@ -65,6 +65,27 @@ class Flight:
             raise ValueError("Seat {} already occupied".format(seat))
         self._seating[row][letter] = passenger
 
+
+    def num_available_seats(self):
+        return sum(sum(1 for seat in row.values() if seat is None)
+            for row in self._seating if row is not None)
+
+    def make_boarding_class(self, card_printer):
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger,seat,self.number(),self._aircraft.getmodel())
+
+
+    def _passenger_seats(self):
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for lettr in seat_letters:
+                passenger = self._seating[row][lettr]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row,lettr))
+
+
+
+
 class Aircraft:
     """
     It flies
--- use_airtravel.py
+++ use_airtravel.py
@@ -3,20 +3,42 @@ use flight class
 """
 from airtravel import Flight, Aircraft
 
+def make_flight():
+    a1 = Aircraft("blah", "airbus", 22, 6)
+    flight = Flight("SN066", a1)
+    print(flight, type(flight))
+    print(flight.number())
+    flight.alllocate_seat("1C", "John")
+    flight.alllocate_seat("11C", "Cindy")
+    return flight
+
+
+
+
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = "| Name: {0}" \
+              "Flight: {1}" \
+              " Seat: {2}"\
+                " Aircraft {3}"\
+               " |".format(passenger,flight_number,seat,aircraft)
+    banner = "+" + "-" * (len(output)-2) + "+"
+    border = "|" + " " * (len(output) -2) + "|"
+    lines = [banner,border,output,border,banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()
 def main():
     """
 
     :return:
     """
-    a1 = Aircraft("blah", "airbus", 22, 6)
-    f = Flight("SN066",a1)
-    f2 = Flight('SN013',a1)
+    flight = make_flight()
+    print(flight._seating)
+    print(flight.num_available_seats())
+    flight.make_boarding_class(console_card_printer)
 
-    print(f, type(f))
-    print(f.number())
-    print(f2.number())
-    f.alllocate_seat("1C", "John")
-    f.alllocate_seat("11C", "Cindy")
+
+
 if __name__ == '__main__':
     main()
     exit(0)
